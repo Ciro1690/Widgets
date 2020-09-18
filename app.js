655ce7ad5ba7fb1cc443691c7a091ff0995fba46
Ciro <Ciro16@gmail.com>
2020-09-18 13:27:14 -0600

    added weather

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Clock from './clock.jsx'
+import Tabs from './tabs.jsx'
+import Weather from './Weather.jsx'
 
 function App() {
     return ( 
         <div>
             <Clock />
+            <Weather />
         </div>
     )
 }
